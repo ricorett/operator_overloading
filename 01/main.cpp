31ea90c8498c36d4a91d2da6e8077e3e6fba62e0
Lana <115147586+ricorett@users.noreply.github.com>
2024-02-15 16:39:10 +0300

    finish 01

---
+++ 01/main.cpp
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int main()
+{
+	Fraction f1(4, 3);
+	Fraction f2(6, 11);
+
+	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+	return 0;
+}
---
+++ 01/main.h
@@ -0,0 +1,34 @@
+#include <iostream>
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+    
+    bool operator==(Fraction &f){
+        return((double)(this->numerator_ / this->denominator_) == (double)(f.numerator_ / f.denominator_)) ? true : false;
+    }
+    bool operator!=(Fraction &f){
+        return((double)(this->numerator_ / this->denominator_) != (double)(f.numerator_ / f.denominator_)) ? true : false;
+    }
+    bool operator<(Fraction &f){
+        return((double)(this->numerator_ / this->denominator_) < (double)(f.numerator_ / f.denominator_)) ? true : false;
+    }
+     bool operator>(Fraction &f){
+        return((double)(this->numerator_ / this->denominator_) > (double)(f.numerator_ / f.denominator_)) ? true : false;
+    }
+     bool operator<=(Fraction &f){
+        return((double)(this->numerator_ / this->denominator_) <= (double)(f.numerator_ / f.denominator_)) ? true : false;
+    }
+     bool operator>=(Fraction &f){
+        return((double)(this->numerator_ / this->denominator_) >= (double)(f.numerator_ / f.denominator_)) ? true : false;
+    }
+};
